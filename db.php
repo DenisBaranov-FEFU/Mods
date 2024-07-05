b653356875ddcf6d0c5d30d38ad0b70f181fb782
baranov <baranov-2004@list.ru>
2024-07-06 09:08:50 +1000

    1st comm

---
+++ db.php
@@ -0,0 +1,38 @@
+﻿<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "kali";
+$dbName = "mods";
+
+$link = mysqli_connect($servername, $username, $password);
+
+if (!link) {
+  die("Ошибка подключения: " . mysqli_connection_error());
+}
+
+$sql = "CREATE DATABASE IF NOT EXIST $dbName";
+
+if (!mysqli_query($link, $sql)) {
+  echo "Не удалось создать БД";
+}
+
+mysqli_close($link);
+
+$link = mysqli_connect($servername, $username, $password, $dbName);
+
+$sql = "CREATE TABLE IF NOT EXISTS mods(
+  game  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  rating VARCHAR(10) NOT NULL,
+  downloads VARCHAR(10) NOT NULL
+)";
+
+
+if(!mysqli_qyery($link, $sql)) {
+  echo "Не удалось создать таблицу mods";
+}
+
+mysqli_close($link);
+
+
+?>
