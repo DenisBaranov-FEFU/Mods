f1523a586e316bef3654133df469457872768162
baranov <baranov-2004@list.ru>
2024-07-06 10:49:27 +1000

    Style for AdmPanel fix v2

--- AdmPanel.php
+++ AdmPanel.php
@@ -8,7 +8,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 	
-	<link rel="stylesheet" href="CSS/style.css">
+	<link rel="stylesheet" href="CSS/style_adm.css">
 </head>
 <body>
     <h1>Загрузка модификаций</h1>
