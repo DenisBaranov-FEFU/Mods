b325afefd730b9a521085b2f657c801d0626274e
baranov <baranov-2004@list.ru>
2024-07-06 14:28:04 +1000

    Attempt to make mod_page

--- Mod.php
+++ Mod.php
@@ -22,7 +22,7 @@ mysqli_close($link);
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
-    <title><?php $name ?></title>
+    <title><?php print "$name" ?></title>
 	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
