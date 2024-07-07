35eec471e2a05c6451856d5521ed8018c2e799d0
baranov <baranov-2004@list.ru>
2024-07-07 20:59:38 +1000

    Home page updated

--- AdmPanel.php
+++ AdmPanel.php
@@ -15,7 +15,7 @@
             <div class="row form__reg"><input class="form-control" type="text" name="game" placeholder="Название игры"></div>
             <div class="row form__reg"><input class="form-control" type="text" name="name" placeholder="Название мода"></div>
             <div class="row form__reg"><input class="form-control" type="number" name="rating" placeholder="Рейтинг"></div>
-            <div class="row form__reg"><input class="form-control" type="number" name="down_check" placeholder="Кол-во скачиваний"></div>
+            <div class="row form__reg"><input class="form-control" type="number" name="down_count" placeholder="Кол-во скачиваний"></div>
             <div class="row form__reg"><input class="form-control" type="text" name="discription" placeholder="Описание мода"></div>
             <div class="row form__reg"><input class="form-control" type="url" name="url" placeholder="Ссылка на скачивание"></div>
             <div class="row form__reg"><input class="form-control" type="number" name="size" placeholder="Вес в МБ"></div>
@@ -35,20 +35,20 @@ if (!$link) {
     die('Connection failed: ' . mysqli_connect_error());
 }
 
-if (isset($_POST['submit'])) { // Checking if the submit button was clicked
+if (isset($_POST['submit'])) { // counting if the submit button was clicked
     $game = $_POST['game'];
     $name = $_POST['name'];
     $discription = $_POST['discription'];
     $url = $_POST['url'];
     $rating = $_POST['rating'];
-    $down_check = $_POST['down_check'];
+    $down_count = $_POST['down_count'];
     $size = $_POST['size'];
 
-    if (!$game || !$name || !$url || !$size || !$down_check || !$rating) {
+    if (!$game || !$name || !$url || !$size || !$down_count || !$rating) {
         die('Пожалуйста введите все значения!');
     }
 
-    $sql = "INSERT INTO mods (game, name, url, size, discription, down_check, rating) VALUES ('$game', '$name', '$url', '$size', '$discription', '$down_check', '$rating')";
+    $sql = "INSERT INTO mods (game, name, url, size, discription, down_count, rating) VALUES ('$game', '$name', '$url', '$size', '$discription', '$down_count', '$rating')";
 
     if (!mysqli_query($link, $sql)) {
         echo "Не удалось добавить мод";
@@ -68,20 +68,20 @@ if (!$link) {
     die('Ошибка подключения: ' . mysqli_connect_error());
 }
 
-if (isset($_POST['submit'])) { // Checking if the submit button was clicked
+if (isset($_POST['submit'])) { // counting if the submit button was clicked
     $game = $_POST['game'];
     $name = $_POST['name'];
     $discription = $_POST['discription'];
     $url = $_POST['url'];
     $rating = $_POST['rating'];
-    $down_check = $_POST['down_check'];
+    $down_count = $_POST['down_count'];
     $size = $_POST['size'];
 
-    if (empty($game) || empty($name) || empty($url) || empty($size) || empty($down_check) || empty($rating)) {
+    if (empty($game) || empty($name) || empty($url) || empty($size) || empty($down_count) || empty($rating)) {
         die('Пожалуйста, введите все значения!');
     }
 
-    $sql = "INSERT INTO mods (game, name, url, size, discription, down_check, rating) VALUES ('$game', '$name', '$url', '$size', '$discription', '$down_check', '$rating')";
+    $sql = "INSERT INTO mods (game, name, url, size, discription, down_count, rating) VALUES ('$game', '$name', '$url', '$size', '$discription', '$down_count', '$rating')";
 
     if (!mysqli_query($link, $sql)) {
         echo "Не удалось добавить мод: " . mysqli_error($link);
