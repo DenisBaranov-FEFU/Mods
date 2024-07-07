1cd57085aa21c0c78d4d235f55f869ca412f0a2c
baranov <baranov-2004@list.ru>
2024-07-07 11:28:07 +1000

    CSS log + reg + home

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
     <!-- Link to custom CSS -->
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/auth.css">
 </head>
 <body>
     <h1>Авторизуйтесь!</h1>
